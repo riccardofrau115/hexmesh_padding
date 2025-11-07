96bfb8c4add0967e8be27ccde83708b872b7479f
riccardofrau115 <riccardo.frau1104@gmail.com>
2025-11-07 13:59:20 +0100

    salvataggio hexa singoli

--- main.cpp
+++ main.cpp
@@ -5,52 +5,70 @@
 #include <cinolib/dual_mesh.h>
 
 #include <cinolib/meshes/polyhedralmesh.h>
+#include <cinolib/export_cluster.h>
 
 using namespace cinolib;
 
-void mesh_singularity(
-    const AbstractPolyhedralMesh<> &primal,
-    Polyhedralmesh<> &poly_singularity)
-{
-    std::vector<std::vector<uint>> polys_vec = primal.vector_polys();
-    poly_singularity.clear();
-    // poly_singularity.poly_add(polys_vec[0]);
-    auto poly = polys_vec[0];
-    for (size_t i = 0; i < poly.size(); i++)
-    {
-        //std::cout << "Vertex " << i << " of poly p is vert id " << poly[i] << std::endl;
-        poly_singularity.vert_add(primal.vert(poly[i]));
-    }
-
-}
+// void mesh_singularity(
+//     AbstractPolyhedralMesh<> &m,
+//     DrawablePolyhedralmesh<> &poly_singularity)
+// {
+//     using namespace cinolib;
+//     // std::string path = "/singularities/";
+//     std::vector<std::vector<uint>> polys_vec = m.vector_polys();
+//     std::string filename = "flat_single_";
+//     for (uint i = 0; i < (uint)polys_vec.size(); ++i)
+//     {
+//         // std::cout << "Exporting singularity cluster " << i << std::endl;
+//         poly_singularity.clear();
+//         m.poly_data(i).label = i;
+//         export_cluster(m, (uint)i, poly_singularity);
+//         poly_singularity.updateGL();
+//         // poly_singularity.save((filename + std::to_string(i) + ".mesh").c_str());
+//     }
+// }
 
 int main(int argc, char **argv)
 {
     using namespace cinolib;
 
-    DrawablePolyhedralmesh<> mesh_basis(vec3d_from_serialized_xyz(Flat::verts), Flat::faces, Flat::polys, Flat::winding);
+    DrawablePolyhedralmesh<> m(vec3d_from_serialized_xyz(Flat::verts), Flat::faces, Flat::polys, Flat::winding);
     DrawablePolyhedralmesh<> poly_mesh;
     float ang_thresh = float(to_rad(60.0));
-    mesh_basis.edge_mark_sharp_creases(ang_thresh);
+    m.edge_mark_sharp_creases(ang_thresh);
 
-    dual_mesh(mesh_basis, poly_mesh, true);
-    poly_mesh.translate(vec3d(mesh_basis.bbox().delta_x() * 1.5, 0, 0));
+    dual_mesh(m, poly_mesh, true);
+    poly_mesh.translate(vec3d(m.bbox().delta_x() * 1.5, 0, 0));
     poly_mesh.updateGL();
 
     // singularity extraction
-    DrawablePolyhedralmesh<> poly_singularity;
-    mesh_singularity(poly_mesh, poly_singularity);
-    
-    poly_singularity.translate(vec3d(poly_mesh.bbox().delta_x() * 1.5, 0, 0));
-    poly_singularity.updateGL();
 
+    //std::string filename = "poly.mesh";
+
+    DrawablePolyhedralmesh<> poly_singularity;
+    // mesh_singularity(poly_mesh, poly_singularity);
+
+    for (uint i = 0; i < 4; ++i)
+    {
+        poly_singularity.clear();
+        poly_mesh.poly_data(i).label = i;
+        export_cluster(poly_mesh, i, poly_singularity); 
+        poly_singularity.updateGL();
+        std::cout << "Exporting singularity cluster " << i  << std::endl;
+        poly_singularity.save(("poly" + std::to_string(i) + ".mesh").c_str());
+    }
+
+    // poly_singularity.translate(vec3d(poly_mesh.bbox().delta_x() * 1.5, 0, 0));
 
     GLcanvas gui;
-    VolumeMeshControls<DrawablePolyhedralmesh<>> menu(&poly_mesh, &gui, "Hex Mesh Controls");
-    gui.push(&mesh_basis);
-    gui.push(&poly_mesh);
-    gui.push(&poly_singularity);
-    gui.push(&menu);
 
-    return gui.launch();
+    // VolumeMeshControls<DrawablePolyhedralmesh<>> menu(&m, &gui, "Hex Mesh Controls");
+    // gui.push(&m);
+    // gui.push(&poly_mesh);
+    gui.push(&poly_singularity);
+
+    // gui.push(&menu);
+
+    // gui.launch();
+    return 0;
 }
